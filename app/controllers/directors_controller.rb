82b78ad4b344007e4abd31f7a80c2cb55eb6b004
laurenmlang <laurenmlang@gmail.com>
2015-08-05 14:03:41 -0500

    Updated Directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,8 +5,39 @@ class DirectorsController < ApplicationController
 	end
 
 	def show
-		@id = params[:id]
-		@director = Director.find_by(@id)
-
+		@director = Director.find(params[:id])
 	end
+
+	def new
+	end
+
+	def create
+   		 @director = Director.new
+   		 @director.name = params[:the_name]
+   		 @director.bio = params[:the_bio]
+    	 @director.dob = params[:the_dob]
+    	 @director.image_url = params[:the_image_url]
+    	 @director.save
+    	 render("show")
+	end
+
+	def edit
+		 @director = Director.find(params[:id])
+	end
+
+	def update
+		 @director = Director.find(params[:id])
+   		 @director.name = params[:the_name]
+   		 @director.bio = params[:the_bio]
+    	 @director.dob = params[:the_dob]
+    	 @director.image_url = params[:the_image_url]
+   		 @director.save
+   		 render("show")
+	end
+
+	def delete
+		@director = Director.find(params[:id])
+		@director.destroy
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  
+
+# DIRECTORS:  
+# Create DIRECTORS
+  get('/directors/new', { :controller => 'directors', :action => 'new' })
+  get('/create_director', { :controller => 'directors', :action => 'create' })
+
 # Route to DIRECTORS
 
   get("/directors", {:controller =>"directors", :action => "index"})
   get("/directors/:id", {:controller =>"directors", :action => "show"})
 
+# Update DIRECTORS
+  get('/directors/:id/edit', { :controller => 'directors', :action => 'edit' })
+  get('/update_director/:id', { :controller => 'directors', :action => 'update' })
+
+# Delete DIRECTORS
+ get('/delete_director/:id', { :controller => 'directors', :action => 'delete' })
+
+
+# MOVIES:
 # Create MOVIES
   get('/movies/new', { :controller => 'movies', :action => 'new' })
   get('/create_movie', { :controller => 'movies', :action => 'create' })
@@ -25,5 +39,22 @@ Rails.application.routes.draw do
 
   get("/actors", {:controller =>"actors", :action => "index"})
   get("/actors/:id", {:controller =>"actors", :action => "show"})
+
+# ACTORS:  
+# Create ACTORS
+  get('/actors/new', { :controller => 'actors', :action => 'new' })
+  get('/create_actor', { :controller => 'actors', :action => 'create' })
+
+# Route to ACTORS
+
+  get("/actors", {:controller =>"actors", :action => "index"})
+  get("/actors/:id", {:controller =>"actors", :action => "show"})
+
+# Update ACTORS
+  get('/actors/:id/edit', { :controller => 'actors', :action => 'edit' })
+  get('/update_actor/:id', { :controller => 'actors', :action => 'update' })
+
+# Delete ACTORS
+ get('/delete_actor/:id', { :controller => 'actors', :action => 'delete' })
 
 end
